a5a1fd7cf2cc2bc02db33dcab766bebe5ca73894
wow1324332 <wow1324332@gmail.com>
2026-05-04 08:12:25 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-// vite.config.js 전체 덮어쓰기
+// vite.config.js (이 코드로 덮어씌워주세요)
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // 빌드 시 외부 패키지로 처리하여 에러 무시
-      external: [
-        '@yume-chan/adb',
-        '@yume-chan/adb-backend-webusb',
-        '@yume-chan/stream-extra'
-      ]
-    }
-  },
-  optimizeDeps: {
-    // 개발 서버에서도 문제없이 로드되도록 설정
-    include: [
-      '@yume-chan/adb',
-      '@yume-chan/adb-backend-webusb',
-      '@yume-chan/stream-extra'
-    ]
-  }
 })
